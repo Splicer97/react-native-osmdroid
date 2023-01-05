218e656136deab8c89fa39b3bf3484de5b1cfb91
Nikita_Kovantsov <kovancov@gmail.com>
2023-01-05 20:24:16 +0300

    fix: some fix

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const LINKING_ERROR =
   `The package 'react-native-osmdroid' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  '- You are not using Expo Go';
 
 type OsmdroidProps = {
   style: ViewStyle;
