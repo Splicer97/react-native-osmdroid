0f527632b1ab94e84c67d444dbabafb76429a203
Nikita_Kovantsov <kovancov@gmail.com>
2023-01-06 18:50:19 +0300

    feat: add markers

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import { StyleSheet } from 'react-native';
-import MapView, { Region } from 'react-native-osmdroid';
+import { StyleSheet, Text } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-osmdroid';
 
 export default function App() {
   const initialRegion: Region = {
@@ -10,7 +10,18 @@ export default function App() {
     latitudeDelta: 0.5,
     longitudeDelta: 0.5,
   };
-  return <MapView style={styles.container} initialRegion={initialRegion} />;
+  return (
+    <MapView style={styles.container} initialRegion={initialRegion}>
+      <Marker
+        coordinate={{
+          latitude: 55.75222,
+          longitude: 37.61556,
+        }}
+      >
+        <Text>123</Text>
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { MapView, MapViewProps } from './MapView';
 export type { MapViewProps };
+import Marker from './MapMarker';
+export { MapMarker } from './MapMarker';
 export * from './MapView.types';
 export * from './sharedTypes';
 export default MapView;
+export { Marker };
